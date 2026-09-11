c1847137adf400b3ae9196485d50f0635071d01c
Sydenstricker <sydenstricker.sony@gmail.com>
2026-09-11 12:41:02 -0300

    atualiza build

---
+++ assets/diskStorage-juCr7QoX.js
@@ -0,0 +1,1 @@
+import{a as e,m as t}from"./index-D27xHvb4.js";export{e as readLinguisticProfile,t as writeLinguisticProfile};
--- assets/diskStorage-qKLLgAFr.js
+++
@@ -1,1 +0,0 @@
-import{a as e,m as t}from"./index-BoGjblmV.js";export{e as readLinguisticProfile,t as writeLinguisticProfile};
